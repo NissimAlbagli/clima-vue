63a3aec2a2b315e15d7f8769294d71c1a220f5f7
Nissim Albagli <nissimalbagli@gmail.com>
2024-02-01 00:53:18 -0300

    Componente de alerta y state de error

--- src/components/Formulario.vue
+++ src/components/Formulario.vue
@@ -1,10 +1,13 @@
 <script setup>
-import { reactive } from 'vue';
+import { reactive, ref} from 'vue';
+import Alerta from './Alerta.vue';
 
 const busqueda = reactive({
     ciudad: '',
     pais: ''
 })
+
+const error = ref('');
 
 const paises = [
     { codigo: 'US', nombre: 'Estados Unidos' },
@@ -16,10 +19,25 @@ const paises = [
     { codigo: 'PE', nombre: 'Perú' },
     { codigo: 'CL', nombre: 'Chile' },
 ]
+
+const consultarClima = () => {
+    if(Object.values(busqueda).includes('')) {
+        error.value = 'Todods los campos son obligatorios';
+        return
+    }
+
+    error.value = '';
+}
 </script>
 
 <template>
-    <form class="formulario">
+    <form 
+        class="formulario"
+        @submit.prevent="consultarClima"
+    >
+
+        <Alerta v-if="error">{{ error }}</Alerta>
+
         <div class="campo">
             <label for="ciudad">Ciudad</label>
             <input 
