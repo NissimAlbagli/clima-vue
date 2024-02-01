111fc2f4f01bfed3c7c7814f4bb84e6fd50919e7
Nissim Albagli <nissimalbagli@gmail.com>
2024-02-01 01:15:47 -0300

    Se creo la variable para la llave de API en un env, se importo y se consigio el url, se instalo axios

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,7 +1,13 @@
 export default function useClima() {
 
-    const obtenerClima = () => {
-        console.log('Consultando...');
+    const obtenerClima = ({ ciudad, pais }) => {
+        // Importar API key
+        const key = import.meta.env.VITE_API_KEY;
+
+        // Obtener la latitud y longitud
+        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+        
+        // Obtener el clima
     }
 
     return {
