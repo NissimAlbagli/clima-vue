5fcbd6245a3bf35f6437af1bbddc251711c97991
Nissim Albagli <nissimalbagli@gmail.com>
2024-02-01 01:41:56 -0300

    Se comenxo a crear la logica para hacer los llamados a la API

--- src/composables/useClima.js
+++ src/composables/useClima.js
@@ -1,13 +1,24 @@
+import axios from 'axios';
+
 export default function useClima() {
 
-    const obtenerClima = ({ ciudad, pais }) => {
+    const obtenerClima = async ({ ciudad, pais }) => {
         // Importar API key
         const key = import.meta.env.VITE_API_KEY;
-
-        // Obtener la latitud y longitud
-        const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`
+        try {
+            
+            // Obtener la latitud y longitud
+            const url = `http://api.openweathermap.org/geo/1.0/direct?q=${ciudad},${pais}&limit=1&appid=${key}`;
+            const {data} = await axios(url);
+            const { lat, lon } = data[0];
+            // Obtener el clima
+            const urlClima = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${key}`;
+            const {data: resultado} = await axios(urlClima);
+            console.log(resultado);
+        } catch (error) {
+            console.log(error);
+        }
         
-        // Obtener el clima
     }
 
     return {
